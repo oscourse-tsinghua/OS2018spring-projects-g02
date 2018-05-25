cb93775ccc26048dbf40ebad4895d57d1a3389a3
dzy <daizy15@mails.tsinghua.edu.cn>
2018-05-25 23:46:26 +0800

    add uart random wait simulation

--- cpu0-emulator/uart.c
+++ cpu0-emulator/uart.c
@@ -19,12 +19,19 @@ unsigned uart_feed(struct machine_t* m, unsigned c){
  * pulls a character out from machine (onto emulator display)
  */
 unsigned uart_request(struct machine_t * m, unsigned * rtn){
+  static int wait = UART_INIT_WAIT;
   if (m->regs[REG_FR] & FRBIT_UART1_OUTRDY)
     return 1; // the machine has nothing to emit
-  // the machine has something to emit, pull it out
-  m->regs[REG_FR] |= FRBIT_UART1_OUTRDY;
-  m->regs[REG_FR] |= FRBIT_UART1_OUT;
-  // same as feed, justify use of mem_read?
-  assert(mem_read(m, UART1_OUT, rtn) == 0);
-  return 0;
+  if (wait == -1) {
+    // the machine has something to emit, pull it out
+    m->regs[REG_FR] |= FRBIT_UART1_OUTRDY;
+    m->regs[REG_FR] |= FRBIT_UART1_OUT;
+    // same as feed, justify use of mem_read?
+    wait = UART_RANDOM_WAIT;
+    assert(mem_read(m, UART1_OUT, rtn) == 0);
+    return 0;
+  } else {
+    wait--;
+    return 1;
+  }
 }
